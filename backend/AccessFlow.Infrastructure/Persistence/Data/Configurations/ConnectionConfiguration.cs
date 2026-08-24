4d16c0482fc21b76165876fe90cd30ea37b65921
parhelia <parhelia.16.06@gmail.com>
2026-08-24 23:25:16 +0300

    Add config connections tbl

--- backend/AccessFlow.Infrastructure/Persistence/Data/Configurations/ConnectionConfiguration.cs
+++ backend/AccessFlow.Infrastructure/Persistence/Data/Configurations/ConnectionConfiguration.cs
@@ -8,6 +8,39 @@ public class ConnectionConfiguration : IEntityTypeConfiguration<Connection>
 {
     public void Configure(EntityTypeBuilder<Connection> builder)
     {
+        builder.ToTable("connections");
+
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .UseIdentityByDefaultColumn();
+
+        builder.Property(x => x.IdClient)
+            .HasColumnName("id_client")
+            .IsRequired();
+        builder.HasOne(x => x.Client)
+            .WithMany(x => x.Connections)
+            .HasForeignKey(x => x.IdClient)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(x => x.IdExternal).HasColumnName("id_external").IsRequired();
+        builder.Property(x => x.Name).HasColumnName("name").IsRequired();
+        builder.Property(x => x.ConnectionString).HasColumnName("connection_string").IsRequired();
+        builder.Property(x => x.SubUrl).HasColumnName("sub_url").IsRequired();
+        builder.Property(x => x.Status).HasColumnName("status").IsRequired();
+        builder.Property(x => x.CreatedAt).HasColumnName("created_at").IsRequired();
+        builder.Property(x => x.UpdatedAt).HasColumnName("updated_at").IsRequired();
+
+        builder.HasIndex(x => x.IdExternal)
+            .HasDatabaseName("ux_connections_id_external")
+            .IsUnique();
+        builder.HasIndex(x => x.Name)
+            .HasDatabaseName("ux_connections_name")
+            .IsUnique();
+
+        builder.HasIndex(x => x.SubUrl)
+            .HasDatabaseName("ux_connections_sub_url")
+            .IsUnique();
 
     }
 }
