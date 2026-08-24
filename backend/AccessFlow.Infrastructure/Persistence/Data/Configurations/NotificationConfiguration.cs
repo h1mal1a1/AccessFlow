10ab65e2ad4fccbeb38fa5208805e706b138647e
parhelia <parhelia.16.06@gmail.com>
2026-08-24 23:24:35 +0300

    Add config natificatons tbl

--- backend/AccessFlow.Infrastructure/Persistence/Data/Configurations/NotificationConfiguration.cs
+++ backend/AccessFlow.Infrastructure/Persistence/Data/Configurations/NotificationConfiguration.cs
@@ -8,6 +8,38 @@ public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
 {
     public void Configure(EntityTypeBuilder<Notification> builder)
     {
+        builder.ToTable("notifications");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .UseIdentityByDefaultColumn();
 
+        builder.HasOne(x => x.Client)
+            .WithMany(x => x.Notifications)
+            .HasForeignKey(x => x.IdClient)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Property(x => x.IdClient)
+            .HasColumnName("id_client")
+            .IsRequired();
+
+        builder.HasOne(x => x.Connection)
+            .WithMany(x => x.Notifications)
+            .HasForeignKey(x => x.IdConnection)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Property(x => x.IdConnection)
+            .HasColumnName("id_connection")
+            .IsRequired();
+
+        builder.Property(x => x.Status)
+            .HasColumnName("status")
+            .IsRequired();
+
+        builder.Property(x => x.SendAt)
+            .HasColumnName("send_at");
+
+        builder.Property(x => x.Error)
+            .HasColumnName("error");
     }
 }
