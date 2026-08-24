45260ae403dd145c60db61fb8db4a73bdee9ac72
parhelia <parhelia.16.06@gmail.com>
2026-08-24 23:27:08 +0300

    Add config bulk_operation_items tbl

--- backend/AccessFlow.Infrastructure/Persistence/Data/Configurations/BulkOperationItemConfiguration.cs
+++ backend/AccessFlow.Infrastructure/Persistence/Data/Configurations/BulkOperationItemConfiguration.cs
@@ -8,6 +8,37 @@ public class BulkOperationItemConfiguration : IEntityTypeConfiguration<BulkOpera
 {
     public void Configure(EntityTypeBuilder<BulkOperationItem> builder)
     {
+        builder.ToTable("bulk_operation_items");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .UseIdentityByDefaultColumn();
 
+        builder.HasOne(x => x.BulkOperation)
+            .WithMany(x => x.BulkOperationItems)
+            .HasForeignKey(x => x.IdBulkOperation)
+            .OnDelete(DeleteBehavior.Cascade);
+        builder.Property(x => x.IdBulkOperation)
+            .HasColumnName("id_bulk_operation")
+            .IsRequired();
+
+        builder.HasOne(x => x.Client)
+            .WithMany(x => x.BulkOperationItems)
+            .HasForeignKey(x => x.IdClient)
+            .OnDelete(DeleteBehavior.Cascade);
+        builder.Property(x => x.IdClient)
+            .HasColumnName("id_client")
+            .IsRequired();
+
+        builder.Property(x => x.Status)
+            .HasColumnName("status")
+            .IsRequired();
+
+        builder.Property(x => x.Error)
+            .HasColumnName("error");
+
+        builder.HasIndex(x => new { x.IdBulkOperation, x.IdClient })
+            .HasDatabaseName("ux_bulk_operation_items_operation_client")
+            .IsUnique();
     }
 }
