d8461f7aacb94ec1b86e6664e5d60277f97984a5
parhelia <parhelia.16.06@gmail.com>
2026-08-24 23:25:57 +0300

    Add config bulk_operation_items tbl

--- backend/AccessFlow.Infrastructure/Persistence/Data/Configurations/BulkOperationConfiguration.cs
+++ backend/AccessFlow.Infrastructure/Persistence/Data/Configurations/BulkOperationConfiguration.cs
@@ -8,6 +8,30 @@ public class BulkOperationConfiguration : IEntityTypeConfiguration<BulkOperation
 {
     public void Configure(EntityTypeBuilder<BulkOperation> builder)
     {
+        builder.ToTable("bulk_operation");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .UseIdentityByDefaultColumn();
 
+        builder.Property(x => x.Status)
+            .HasColumnName("status")
+            .IsRequired();
+
+        builder.Property(x => x.UsersProcessed)
+            .HasColumnName("users_processed");
+
+        builder.Property(x => x.SuccessfullyProcessed)
+            .HasColumnName("successfully_processed");
+
+        builder.Property(x => x.ErrorProcessed)
+            .HasColumnName("error_processed");
+
+        builder.Property(x => x.StartAt)
+            .HasColumnName("start_at")
+            .IsRequired();
+
+        builder.Property(x => x.EndAt)
+            .HasColumnName("end_at");
     }
 }
