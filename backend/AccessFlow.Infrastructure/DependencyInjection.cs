89537963b17f2a210f0a080e501e1324c1a982eb
parhelia <parhelia.16.06@gmail.com>
2026-08-24 23:28:01 +0300

    DependencyInjection in infrastructure

---
+++ backend/AccessFlow.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+using AccessFlow.Infrastructure.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AccessFlow.Infrastructure;
+
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection") ??
+            throw new InvalidOperationException("Connection string \'DefaultConnection\' not found");
+
+        services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+        return services;
+    }
+}
